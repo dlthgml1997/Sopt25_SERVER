a4cf8666e6a930b1d1c77c05718db9d2ae5cc5f9
LeeSohee <shfksthgml@naver.com>
2019-11-27 16:50:50 +0900

    comment CRUD 구현

--- homework/homework2/routes/blogs/articles/comments/comment.js
+++ homework/homework2/routes/blogs/articles/comments/comment.js
@@ -2,144 +2,105 @@ const express = require('express');
 const router = express.Router({
     mergeParams: true
 });
-//const statusCode = require('../../../../module/utils/statusCode');
-//const responseMessage = require('../../../../module/utils/responseMessage');
+const statusCode = require('../../../../module/utils/statusCode');
+const responseMessage = require('../../../../module/utils/responseMessage');
 const authUtil = require('../../../../module/utils/authUtil');
-
-// const Comment = require('../../../../model/Comments');
+const pool = require('../../../../module/pool')
 
 const THIS_LOG = '댓글';
 /*
     [GET] localhost/blogs/${blogIdx}/articles/${articleIdx}/comments
     댓글 전체 보기
 */
-router.get('/', (req, res) => {
-    /*
-        TODO 1 Model에서 값 받아오기
-        동기 or 비동기 자유롭게 구현
-    */
-    Comment.readAll();
-    /* 
-        TODO 2 결과값 출력
-        result는 sample 입니다!
-    */
-    const result = [{
-        commentIdx: 0,
-        writer: '윤희성',
-        content: 'good :)',
-        articleIdx
-    }];
+router.get('/', async (req, res) => {
+    const selectSql = 'SELECT * FROM comment WHERE articleIdx= ?';
+    const result = await pool.queryParam_Arr(selectSql,[req.params.articleIdx]);
+    if(!result){
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(responseMessage.INTERNAL_SERVER_ERROR);
+        return;
+    }
     res.status(statusCode.OK).send(authUtil.successTrue(
-        responseMessage.X_READ_ALL_SUCCESS(THIS_LOG),
+        responseMessage.COMMENT_READ_ALL_SUCCESS,
         result));
+        
 });
 /*
     [GET] localhost/blogs/${blogIdx}/articles/${articleIdx}/comments/${commentIdx}
     댓글 하나보기
 */
-router.get('/:commentIdx', (req, res) => {
+router.get('/:commentIdx', async (req, res) => {
     const {
+        articleIdx,
         commentIdx
     } = req.params;
-    // TODO 1 parameter null check
-
-    /*
-        TODO 2 Model에서 값 받아오기
-        동기 or 비동기 자유롭게 구현
-    */
-    Comment.read(commentIdx);
-    /* 
-        TODO 3 결과값 출력
-        result는 sample 입니다!
-    */
-    const result = {
-        commentIdx,
-        writer: '윤희성',
-        content: 'good :)',
-        articleIdx
-    };
+    if(!commentIdx || articleIdx){
+        res.status(statusCode.BAD_REQUEST).send(responseMessage.NULL_VALUE);
+        return;
+    }
+    const selectSql = 'SELECT * FROM comment WHERE idx = ? AND articleIdx = ?'
+    const result = await pool.queryParam_Arr(selectSql,[commentIdx,articleIdx]);
+    if(!result){
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(responseMessage.INTERNAL_SERVER_ERROR);
+        return;
+    }
     res.status(statusCode.OK).send(authUtil.successTrue(
-        responseMessage.X_READ_SUCCESS(THIS_LOG),
+        responseMessage.COMMENT_READ_SUCCESS,
         result));
 });
 /*
     [POST] localhost/blogs/${blogIdx}/articles/${articleIdx}/comments
     댓글 생성하기
 */
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const {
         articleIdx
     } = req.params;
     // TODO 1 parameter null check
-    const {} = req.body;
-    const json = {};
-    /*
-        TODO 2 Model에서 값 받아오기
-        동기 or 비동기 자유롭게 구현
-    */
-    Comment.create(json);
-    /* 
-        TODO 3 결과값 출력
-        result는 sample 입니다!
-    */
-    const result = {
-        commentIdx: 0,
-        writer: '윤희성',
-        content: 'good :)',
-        articleIdx
-    };
+    if(!articleIdx){
+        res.status(statusCode.BAD_REQUEST).send(responseMessage.NULL_VALUE);
+        return;
+    }
+    const insertSql = 'INSERT INTO comment(writer,contents,articleIdx) VALUES(?, ?, ?)';
+    const result = await pool.queryParam_Arr(insertSql,[req.body.writer,req.body.contents,articleIdx]);
+    if(!result){
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(responseMessage.INTERNAL_SERVER_ERROR);
+        return;
+    }
     res.status(statusCode.OK).send(authUtil.successTrue(
-        responseMessage.X_CREATE_SUCCESS(THIS_LOG),
+        responseMessage.COMMENT_CREATE_SUCCESS,
         result));
 });
 /*
     [PUT]  localhost/blogs/${blogIdx}/articles/${articleIdx}/comments
     댓글 수정하기
 */
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
     const {
         articleIdx
     } = req.params;
-    // TODO 1 parameter null check
-    const {} = req.body;
-    const json = {};
-    /*
-        TODO 2 Model에서 값 받아오기
-        동기 or 비동기 자유롭게 구현
-    */
-    Comment.update(json);
-    /* 
-        TODO 3 결과값 출력
-        result는 sample 입니다!
-    */
-    const result = {
-        commentIdx: 0,
-        writer: '윤희성',
-        content: 'good :)',
-        articleIdx
-    };
+    if(!articleIdx){
+        res.status(statusCode.BAD_REQUEST).send(responseMessage.NULL_VALUE);
+        return;
+    }
+    const updateSql = 'UPDATE comment SET writer= ?, contents = ? WHERE articleIdx = ? AND idx = ?';
+    const result = await pool.queryParam_Arr(updateSql,[req.body.writer,req.body.contents,articleIdx,req.body.idx]);
+    if(!result){
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(responseMessage.INTERNAL_SERVER_ERROR);
+        return;
+    }
     res.status(statusCode.OK).send(authUtil.successTrue(
-        responseMessage.X_UPDATE_SUCCESS(THIS_LOG),
+        responseMessage.COMMENT_UPDATE_SUCCESS,
         result));
 });
 /*
     [DELETE] localhost/blogs/${blogIdx}/articles/${articleIdx}/comments
     댓글 삭제하기
 */
-router.delete('/', (req, res) => {
-    // TODO 1 parameter null check
-    const {} = req.body;
-    const json = {};
-    /*
-        TODO 2 Model에서 값 받아오기
-        동기 or 비동기 자유롭게 구현
-    */
-    Comment.delete(json);
-    /* 
-        TODO 3 결과값 출력
-        result는 sample 입니다!
-    */
+router.delete('/', async (req, res) => {
+    const deleteSql = 'DELETE FROM comment WHERE articleIdx = ? AND idx = ?';
+    await pool.queryParam_Arr(deleteSql,[req.params.articleIdx,req.body.idx]);
+
     res.status(statusCode.OK).send(authUtil.successTrue(
-        responseMessage.X_DELETE_SUCCESS(THIS_LOG)));
+        responseMessage.COMMENT_DELETE_SUCCESS));
 });
 module.exports = router;
